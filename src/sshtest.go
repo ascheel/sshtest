04446a82d02881a15c795a90a75a104d1c016c32
scheel <scheel@adobe.com>
2023-10-17 14:15:00 -0600

    json started

--- src/sshtest.go
+++ src/sshtest.go
@@ -1,32 +1,43 @@
 package main
 
 import (
-	"os"
 	"log"
+	"os"
+	"strings"
+
 	"golang.org/x/crypto/ssh"
+
 	//"golang.org/x/crypto/ssh/knownhosts"
-	"path/filepath"
 	"bufio"
-	"io"
 	"fmt"
+	"io"
+	"path/filepath"
+
 	//"strconv"
-	"net"
 	"errors"
+	"net"
 	"syscall"
 	"time"
+
 	//"strings"
+	"crypto/sha256"
+	"encoding/json"
 	"reflect"
 )
 
-type SSH struct {
-	host     string
-	user     string
-	port     string
-	key      string
-	password string
-	errCode  uint8
-	errText  string
-	errRaw   error
+type connectionInfo struct {
+	Host     string
+	User     string
+	Port     string
+	Key      string
+	Password string
+	ErrCode  int
+	ErrText  string
+	ErrRaw   error
+}
+
+func (connInfo *connectionInfo) SetDefaults() {
+	connInfo.Port = "22"
 }
 
 func check(e error) {
@@ -54,64 +65,47 @@ func readFile(filename string) []byte {
 	return bs
 }
 
-func sshConnectWithPKey(connInfo *SSH) (*ssh.Client, *ssh.Session, error) {
-	hostString := net.JoinHostPort(connInfo.host, connInfo.port)
-	pKey := readFile(connInfo.key)
-	signer, err := ssh.ParsePrivateKey(pKey)
-	check(err)
+func sshConnect(connInfo *connectionInfo) (*ssh.Client, *ssh.Session, error) {
+	var auth     []ssh.AuthMethod
 
-	//hostKeyCallback, err := knownhosts.New(knownHostsFile)
-	//check(err)
+	hostString := net.JoinHostPort(connInfo.Host, connInfo.Port)
 
 	timeout, _ := time.ParseDuration("5s")
-
-	conf := &ssh.ClientConfig{
-		User: connInfo.user,
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
-		Timeout: timeout,
+	if len(connInfo.Password) == 0 && len(connInfo.Key) == 0 {
+		log.Fatalln("Both key and password are empty.  One must be provided.")
+	} else if len(connInfo.Key) > 0 {
+		keyData := readFile(connInfo.Key)
+		pKey, keyErr := ssh.ParsePrivateKey(keyData)
+		if keyErr != nil {
+			log.Fatalln("Bad key: " + connInfo.Key)
+		}
+		auth = []ssh.AuthMethod{ssh.PublicKeys(pKey)}
+	} else if len(connInfo.Password) > 0 {
+		auth = []ssh.AuthMethod{ssh.Password(connInfo.Password)}
 	}
 
-	conf.HostKeyCallback = ssh.InsecureIgnoreHostKey()
+	conf := &ssh.ClientConfig{
+		User: connInfo.User,
+		Auth: auth,
+		Timeout: timeout,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
 
-	client, err := ssh.Dial("tcp", hostString, conf)
+	client, err := ssh.Dial(
+		"tcp",
+		hostString,
+		conf,
+	)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	session, err := client.NewSession()
 	if err != nil {
-		fmt.Println("Failure with client.NewSession()")
 		return nil, nil, err
 	}
 
 	return client, session, nil
-}
-
-func tryConnectWithPrivateKey(connInfo *SSH) bool {
-	if len(connInfo.port) == 0 {
-		connInfo.port = "22"
-	}
-
-	client, session, err := sshConnectWithPKey(connInfo)
-	if err != nil {
-		//connInfo.errRaw = err
-		return false
-	}
-	command := "ls -l /"
-	_, err = session.CombinedOutput(command)
-	if err != nil {
-		//connInfo.errRaw = err
-		return false
-	}
-	if client == nil || session == nil {
-		log.Fatal("You shouldn't get here.")
-		return false
-	}
-	connInfo.errCode = 0
-	connInfo.errText = "success"
-	return true
 }
 
 func sshCheck(err error) {
@@ -122,39 +116,120 @@ func sshCheck(err error) {
 	}
 }
 
-func tryConnect(connInfo *SSH) {
-	if len(connInfo.key) == 0 && len(connInfo.password) == 0 {
+func tryConnect(connInfo *connectionInfo) bool {
+	if len(connInfo.Key) == 0 && len(connInfo.Password) == 0 {
 		log.Fatalln("No key or password provided.")
-	} else if len(connInfo.key) > 0 && len(connInfo.password) > 0 {
+	} else if len(connInfo.Key) > 0 && len(connInfo.Password) > 0 {
 		log.Fatalln("Application does not yet support passwords AND keys both.")
-	} else if len(connInfo.host) == 0 {
+	} else if len(connInfo.Host) == 0 {
 		log.Fatalln("No host provided.")
-	} else if len(connInfo.port) == 0 {
-		connInfo.port = "22"
 	}
-	if len(connInfo.key) > 0 {
-		// Connect with key
-		tryConnectWithPrivateKey(connInfo)
-	} else if len(connInfo.password) > 0 {
-		// Connect with password
-		//return tryConnectWithPassword(connInfo)
+	client, session, err := sshConnect(connInfo)
+	connInfo.ErrRaw = err
+	if err != nil {
+		return false
 	}
+
+	command := "ls -l /"
+	_, err = session.CombinedOutput(command)
+	if err != nil {
+		return false
+	}
+	if client == nil || session == nil {
+		log.Fatal("You shouldn't get here.")
+		return false
+	}
+	return true
 }
 
-func printStruct(connInfo *SSH) {
+func printStruct(connInfo *connectionInfo) {
 	s := reflect.ValueOf(&connInfo).Elem().Elem()
 	typeOfSSH := s.Type()
-	fmt.Println(typeOfSSH)
+	//fmt.Println(typeOfSSH)
 	for i := 0; i < s.NumField(); i++ {
 	 	f := s.Field(i)
-		fmt.Println(f)
+		//fmt.Println(f)
 		fmt.Printf("%d: %s %s = %v\n", i, typeOfSSH.Field(i).Name, f.Type(), f.Interface())
 	}
 }
 
-func handleExit(connInfo *SSH) {
+func getJson(connInfo *connectionInfo) string {
+	type e struct {
+		Host string
+		Port string
+		User string
+		Password string
+		Key string
+		ErrCode int
+		ErrText string
+	}
+	obj := e{
+		Host: connInfo.Host,
+		Port: connInfo.Port,
+		User: connInfo.User,
+		Password: sha256sum(connInfo.Password),
+		Key: connInfo.Key,
+		ErrCode: connInfo.ErrCode,
+		ErrText: connInfo.ErrText,
+	}
+	jsonData, err := json.MarshalIndent(obj, "", "    ")
+	if err != nil {
+		log.Fatalln("Failed to parse json output.")
+	}
+	return string(jsonData)
+}
+
+func handleExit(connInfo *connectionInfo) {
+	// 0 = success
+	// 1 = authentication failure
+	// 2 = timeout
+	// 3 = connection refused
+	// 4 = no route to host
+	// 5 = can't resolve host
+
 	printStruct(connInfo)
 	fmt.Printf("%+v\n", connInfo)
+	if connInfo.ErrRaw == nil {
+		connInfo.ErrCode = 0
+		connInfo.ErrText = "success"
+	} else {
+		if isAuthenticationError(connInfo.ErrRaw) {
+			connInfo.ErrCode = 1
+			connInfo.ErrText = "authentication failure"
+		} else if os.IsTimeout(connInfo.ErrRaw) {
+			connInfo.ErrCode = 2
+			connInfo.ErrText = "timeout"
+		} else if errors.Is(connInfo.ErrRaw, syscall.ECONNREFUSED) {
+			connInfo.ErrCode = 3
+			connInfo.ErrText = "connection refused"
+		} else {
+			connInfo.ErrCode = 255
+			connInfo.ErrText = "Unknown"
+		}
+	}
+	fmt.Println(getJson(connInfo))
+	os.Exit(connInfo.ErrCode)
+}
+
+func isAuthenticationError(err error) bool {
+	authKeywords := []string{
+		"authentication failed",
+		"invalid credentials",
+		"unable to authenticate",
+		"no supported methods remain",
+	}
+	for _, keyword := range authKeywords {
+		if strings.Contains(strings.ToLower(string(fmt.Sprint(err))), strings.ToLower(keyword)) {
+			return true
+		}
+	}
+	return false
+}
+
+func sha256sum(text string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(text))
+	return fmt.Sprintf("%x", hasher.Sum(nil))
 }
 
 func main() {
@@ -162,18 +237,21 @@ func main() {
 	check(err)
 
 	sshdir := filepath.Join(homedir, ".ssh")
-	pKeyPath := filepath.Join(sshdir, "id_rsa_legion")
+	pKeyPath := filepath.Join(sshdir, "id_rsa_ea")
 
-	connInfo := SSH {
-		host: "192.168.1.15",
-		user: "art",
-		port: "22",
-		key: pKeyPath,
-		password: "",
+	connInfo := connectionInfo {
+		// Host: "192.168.1.15",
+		Host: "10.43.1.1",
+		// Host: "jump.dev-va6.ea.adobe.net",
+		// Host: "192.168.1.2",
+		User: "ea",
+		Port: "22",
+		Key: pKeyPath,
+		Password: "",
 	}
 
 	tryConnect(&connInfo)
-	handleExit(connInfo)
+	handleExit(&connInfo)
 
 	// if success {
 	// 	fmt.Println("Successful.")
